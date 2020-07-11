848fc6b297dc288eb586e79d7ef499e183215499
cientista1 <40244138+cientista1@users.noreply.github.com>
2020-07-11 15:11:18 -0300

    Updates

---
+++ precache-manifest.620b7301d17c05153b422ea27986b206.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e9218c2ba08ae47a7be0717368b0cc3c",
+    "url": "/tindevfrontend/index.html"
+  },
+  {
+    "revision": "e1326c5f8ff8981f2fea",
+    "url": "/tindevfrontend/static/css/main.f3ade6c3.chunk.css"
+  },
+  {
+    "revision": "c8ede98c89cc64818220",
+    "url": "/tindevfrontend/static/js/2.050058fa.chunk.js"
+  },
+  {
+    "revision": "b67bc97fd0dd67004287af4028afc1b1",
+    "url": "/tindevfrontend/static/js/2.050058fa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e1326c5f8ff8981f2fea",
+    "url": "/tindevfrontend/static/js/main.070c501f.chunk.js"
+  },
+  {
+    "revision": "3f169b8b0c58cf3d212a",
+    "url": "/tindevfrontend/static/js/runtime-main.48f6c210.js"
+  },
+  {
+    "revision": "d4ec34e81346484c83328c449a35141b",
+    "url": "/tindevfrontend/static/media/dislike.d4ec34e8.svg"
+  },
+  {
+    "revision": "542059a425f4220819edb0e56c735bb7",
+    "url": "/tindevfrontend/static/media/like.542059a4.svg"
+  },
+  {
+    "revision": "b9553b9b5bfe62f9d762db551be7b29e",
+    "url": "/tindevfrontend/static/media/load.b9553b9b.gif"
+  },
+  {
+    "revision": "6a01ceef657ab22cc670a0a1ae108176",
+    "url": "/tindevfrontend/static/media/logo.6a01ceef.svg"
+  },
+  {
+    "revision": "92d8d8f46c6fc696ee39b4e842032800",
+    "url": "/tindevfrontend/static/media/match.92d8d8f4.png"
+  }
+]);
